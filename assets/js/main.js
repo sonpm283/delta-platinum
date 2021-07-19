fac11fe5ac06eecf8f27ed06859d788706f20b51
Your Name <you@example.com>
2021-07-20 00:19:38 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
     menuMobile.init()
+    mainSlide.init()
    
 });
 
 // main slide
-
+const navText = ["<i class='bx bx-chevron-left'></i>","<i class='bx bx-chevron-right'></i>" ];
+const mainSlide = {
+    init : function () {
+        this.mainSlide();
+    },
+    mainSlide: function() {
+        $("#home-slide").owlCarousel({
+            items: 1,
+            dots: false,
+            loop: true,
+            nav: false,
+            autoplay: false,
+        })
+    }
+}
 
 // menu mobile 
 const menuMobile = {
