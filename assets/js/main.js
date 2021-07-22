d54b7217e9ed0a3f67830f02083f213787ea6ba8
Your Name <you@example.com>
2021-07-22 07:45:11 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,28 +247,12 @@ const newsSlide = {
   },
 };
 
-var bigimage = $("#big");
+
 var thumbs = $("#thumbs");
-var syncedSecondary = true;
 
-bigimage
-  .owlCarousel({
-    items: 1,
-    slideSpeed: 2000,
-    nav: false,
-    autoplay: false,
-    dots: false,
-    loop: true,
-    responsiveRefreshRate: 200,
-    navText: navText,
-  })
-  .on("changed.owl.carousel", syncPosition);
 
-thumbs
-  .on("initialized.owl.carousel", function () {
-    thumbs.find(".owl-item").eq(0).addClass("current");
-  })
-  .owlCarousel({
+
+thumbs.owlCarousel({
     items: 3,
     dots: false,
     nav: true,
@@ -289,48 +273,6 @@ thumbs
         },
       },
   })
-  .on("changed.owl.carousel", syncPosition2);
-
-function syncPosition(el) {
-  var count = el.item.count - 1;
-  var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
-
-  if (current < 0) {
-    current = count;
-  }
-  if (current > count) {
-    current = 0;
-  }
-  //to this
-  thumbs
-    .find(".owl-item")
-    .removeClass("current")
-    .eq(current)
-    .addClass("current");
-  var onscreen = thumbs.find(".owl-item.active").length - 1;
-  var start = thumbs.find(".owl-item.active").first().index();
-  var end = thumbs.find(".owl-item.active").last().index();
-
-  if (current > end) {
-    thumbs.data("owl.carousel").to(current, 100, true);
-  }
-  if (current < start) {
-    thumbs.data("owl.carousel").to(current - onscreen, 100, true);
-  }
-}
-
-function syncPosition2(el) {
-  if (syncedSecondary) {
-    var number = el.item.index;
-    bigimage.data("owl.carousel").to(number, 100, true);
-  }
-}
-
-thumbs.on("click", ".owl-item", function (e) {
-  e.preventDefault();
-  var number = $(this).index();
-  bigimage.data("owl.carousel").to(number, 300, true);
-});
 
 
 const backTop = {
@@ -348,7 +290,22 @@ const backTop = {
     }
 }
 
-$('#example-2').elevateZoom({
-    zoomType : "inner",
-    cursor: "crosshair"
-});
+
+
+const thumb = $(".thumb-img")
+const thumbBig = $(".thumb-big")
+
+thumb.click(function() {
+    thumbBig.hide()
+    const idThumbBig = $(this).attr("data-thumb-big")
+    $(idThumbBig).show()
+    const imgUrl = $(this).attr("src")
+    thumbBig.attr({"src":imgUrl, "data-zoom-image":imgUrl})
+    
+    $(idThumbBig).elevateZoom({
+        zoomType : "inner",
+        cursor: "crosshair"
+    });
+})
+
+$("#thumbs .item:first img").click()
